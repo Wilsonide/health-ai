551c24bffb3fba93ec5b722bdd060b516e825607
Wilson Icheku <ichekuwilson538@gmail.com>
2025-10-31 23:53:45 +0100

    fix: simple fixes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from rpc import handle_rpc
 
 from cache import (
     add_tip_to_history,
