d8a888b919c4e393022fa1d57af7089315c02c6f
Wilson Icheku <ichekuwilson538@gmail.com>
2025-11-02 15:30:04 +0100

    fix: hot fixes

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ async def message(request: Request):
         rpc_result = RpcResult(
             id=str(uuid.uuid4()),
             contextId=str(rpc_request.id or uuid.uuid4()),
-            kind="message",
+            kind="task",
             status=ResponseStatus(
                 state="completed",
                 timestamp=datetime.now(UTC).isoformat(),
